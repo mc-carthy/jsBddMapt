4e5799d28230f39024684b77a5ea8a43f4817524
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-27 21:58:25 +0100

    Refactor by extracting validation rules into functions

--- validator/lib/validator.js
+++ validator/lib/validator.js
@@ -1,13 +1,25 @@
-module.exports = function (n) {
-    var result = [];
+function nonpositiveValidationRule(n, result) {
     if (n <= 0) {
         result.push('error.nonpositive');
     }
+}
+
+function notDivisibleBy3ValidationRule(n, result) {
     if (n % 3 === 0) {
         result.push('error.three');
     }
+}
+
+function notDivisibleBy5ValidationRule(n, result) {
     if (n % 5 === 0) {
         result.push('error.five');
     }
+}
+
+module.exports = function (n) {
+    var result = [];
+    nonpositiveValidationRule(n, result);
+    notDivisibleBy3ValidationRule(n, result);
+    notDivisibleBy5ValidationRule(n, result);
     return result;
 }
