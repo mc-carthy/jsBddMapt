35da65122416bd776cbf95f1cbe8b03b34e965e4
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-27 21:36:00 +0100

    Now checking for divisible by three numbers

--- validator/lib/validator.js
+++ validator/lib/validator.js
@@ -2,5 +2,8 @@ module.exports = function (n) {
     if (n <= 0) {
         return ['error.nonpositive'];
     }
+    if (n % 3 === 0) {
+        return ['error.three'];
+    }
     return [];
 }
--- validator/test/validator-spec.js
+++ validator/test/validator-spec.js
@@ -4,15 +4,25 @@ var validator = require('../lib/validator');
 
 describe('A Validator', function () {
     it('will return no errors for valid numbers', function () {
-        expect(validator(3)).to.be.empty;
+        expect(validator(7)).to.be.empty;
     });
 
     describe('will return error.nonpositive for not strictly positive numbers:', function () {
-        it('like 0', function() {
+        it('like 0', function () {
             expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
         });
-        it('like -2', function() {
+        it('like -2', function () {
             expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+        });
+    });
+
+    describe('will return error.three for divisible by 3 numbers:', function () {
+        it('like 3', function () {
+            expect(validator(3)).to.be.deep.equal(['error.three']);
+        });
+
+        it('like 6', function () {
+            expect(validator(6)).to.be.deep.equal(['error.three']);
         });
     });
 });
