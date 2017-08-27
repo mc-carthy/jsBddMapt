343a2066b2ec53e3f81278d9df85b2b0c86947a0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-27 21:31:31 +0100

    Create basic validator

--- validator/test/validator-spec.js
+++ validator/test/validator-spec.js
@@ -2,8 +2,16 @@ var chai = require('chai');
 var expect = chai.expect;
 var validator = require('../lib/validator');
 
-describe('A Validator', function() {
-  it('will return error.nonpositive for not strictly positive numbers', function() {
-    expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
-  });
+describe('A Validator', function () {
+    it('will return no errors for valid numbers', function () {
+        expect(validator(3)).to.be.empty;
+    });
+
+    it('will return error.nonpositive for not strictly positive numbers', function () {
+        expect(validator(0)).to.be.deep.equal(['error.nonpositive']);
+    });
+
+    it('will return error.nonpositive for not strictly positive numbers, like -2', function () {
+        expect(validator(-2)).to.be.deep.equal(['error.nonpositive']);
+    });
 });
