9941bbe538538ff9768cadbbd07b1c3a140a3f22
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-27 21:22:15 +0100

    First test

--- validator/test/validator-spec.js
+++ validator/test/validator-spec.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
+var validator = require('../lib/validator');
 
-describe('Be welcome to Mocha', function() {
-  it('with a failing test', function() {
-    assert(false, 'Hello World');
+describe('A Validator', function() {
+  it('will return error.nonpositive for not strictly positive numbers', function() {
+    assert.deepEqual(validator(0), ['error.nonpositive']);
   });
 });
