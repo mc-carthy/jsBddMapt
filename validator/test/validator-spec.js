5058b6cb2b51f4a6d8cd0bc8e91cd166cf729645
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-28 13:15:25 +0100

    Refactor using paramatised tests

--- validator/test/validator-spec.js
+++ validator/test/validator-spec.js
@@ -2,37 +2,29 @@ var chai = require('chai');
 var expect = chai.expect;
 var validator = require('../lib/validator/validator');
 
+function expectedToInclueErrorWhenInvalid(number, error) {
+    it('like ' + number, function() {
+        expect(validator(number)).to.include(error);
+    });
+}
+
 describe('A Validator', function () {
     it('will return no errors for valid numbers', function () {
         expect(validator(7)).to.be.empty;
     });
 
     describe('will return error.nonpositive for not strictly positive numbers:', function () {
-        it('like 0', function () {
-            expect(validator(0)).to.include('error.nonpositive');
-        });
-        it('like -2', function () {
-            expect(validator(-2)).to.include('error.nonpositive');
-        });
+        expectedToInclueErrorWhenInvalid(0, 'error.nonpositive');
+        expectedToInclueErrorWhenInvalid(-2, 'error.nonpositive');
     });
 
     describe('will return error.three for divisible by 3 numbers:', function () {
-        it('like 3', function () {
-            expect(validator(3)).to.include('error.three');
-        });
-
-        it('like 6', function () {
-            expect(validator(15)).to.include('error.three');
-        });
+        expectedToInclueErrorWhenInvalid(3, 'error.three');
+        expectedToInclueErrorWhenInvalid(15, 'error.three');
     });
 
     describe('will return error.five for divisible by 5 numbers:', function () {
-        it('like 5', function () {
-            expect(validator(5)).to.include('error.five');
-        });
-
-        it('like 15', function () {
-            expect(validator(15)).to.include('error.five');
-        });
+        expectedToInclueErrorWhenInvalid(5, 'error.five');
+        expectedToInclueErrorWhenInvalid(15, 'error.five');
     });
 });
